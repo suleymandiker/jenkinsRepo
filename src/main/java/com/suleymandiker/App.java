2ac77419e92308b121eef05b1ffcaf7314f6cf73
SULEYMAN DIKER <TCSDIKER@turkcell.entp.tgc>
2025-11-02 19:29:54 +0300

    added mvn files

---
+++ src/main/java/com/suleymandiker/App.java
@@ -0,0 +1,13 @@
+package com.suleymandiker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
